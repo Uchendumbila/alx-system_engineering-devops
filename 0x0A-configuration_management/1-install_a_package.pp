b044bcebfc5dfa70b532bf811863c91a0109fa0a
Uchendumbila <ekeuche1408@gmail.com>
2024-03-27 23:52:50 +0100

    Task-1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 # install puppet-lint using Puppet
 
-package { 'puppet-lint':
-  ensure   => '2.1.0',
-  provider => 'gem',
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command     => '/usr/bin/pip3 install flask==2.1.0',
+  path        => ['/usr/bin'],
+  refreshonly => true,
 }
