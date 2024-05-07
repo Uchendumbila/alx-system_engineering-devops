cc521d8ad4c4901f844a740f352e5a17ba84972c
Uchendumbila <ekeuche1408@gmail.com>
2024-05-07 22:03:47 +0100

    Task-0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-
 """
-Script that queries subscribers on a given Reddit subreddit
+Script that queries subscribers on a given Reddit subreddit.
 """
 
 import requests
 
+
 def number_of_subscribers(subreddit):
 
-    """Return the total number of subscribers on a given subreddit"""
+    """Return the total number of subscribers on a given subreddit."""
 
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
