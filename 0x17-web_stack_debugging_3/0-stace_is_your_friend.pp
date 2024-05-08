d16a2d456e40615e91446a2094014efbdeeb310a
Uchendumbila <ekeuche1408@gmail.com>
2024-05-08 23:15:39 +0100

    Task_Strace

---
+++ 0x17-web_stack_debugging_3/0-stace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fixing Apache 500 and automating using Puppet
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,1 @@
+This is the RERADME file for the 0x17 Web Stack Debugging III
