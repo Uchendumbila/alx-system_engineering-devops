69498872f1d949a0f4c7dce54f0ebeee4591417f
Uchendumbila <ekeuche1408@gmail.com>
2024-05-08 01:49:43 +0100

    Task-1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,26 +10,15 @@ def top_ten(subreddit):
     '''
         returns the top ten posts for a given subreddit
     '''
-    user_agent = {'User-Agent': 'Lizzie'}
-    url = f'https://www.reddit.com/r/{subreddit}/hot/.json?limit=10'
-    response = requests.get(url, headers=user_agent)
-
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        try:
-            data = response.json()
-            children = data.get('data', {}).get('children', [])
-            for post in children:
-                print(post.get('data', {}).get('title'))
-        except ValueError:
-            print("Invalid JSON response from Reddit API")
-    else:
-        print(f"Error: HTTP status code {response.status_code}")
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                        .format(subreddit), headers=user).json()
+    try:
+        for post in url.get('data').get('children'):
+            print(post.get('data').get('title'))
+    except ValueError:
+        print(None)
 
 
-if __name__ == "__main__":
-    if len(argv) < 2:
-        print("Usage: python script_name.py subreddit_name")
-    else:
-        top_ten(argv[1])
+if __name__ == "__main__":                                                          top_ten(argv[1])
 
