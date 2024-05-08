751a16c47f9a75e842957aed364850329971644e
Uchendumbila <ekeuche1408@gmail.com>
2024-05-08 23:17:50 +0100

    Task_Strace

--- 0x17-web_stack_debugging_3/0-stace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-stace_is_your_friend.pp
@@ -1,6 +1,6 @@
-# Fixing Apache 500 and automating using Puppet
-
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+# fixes Apache 500 error by fixing typo in wordpress
+exec { 'fix typo':
+  onlyif  => 'test -e /var/www/html/wp-settings.php',
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
